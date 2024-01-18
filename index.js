bb36f0c6d9ac79c0937e85b0247cf6f5d6ad67c1
Chesterr94 <49114539+Chesterr94@users.noreply.github.com>
2024-01-19 00:17:51 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ console.log(productPrice);
 console.log(productName,'Це',productCategory);
 console.warn(productCount*productPrice); 
 */
+/*
 const productName = prompt('Введите Наименование товара');
 const productCount = + prompt('Введите количество товара');
 const productCategory = prompt('Введите категорию товара');
@@ -25,4 +26,42 @@ console.log(productPrice);
 console.log(typeof productPrice);
 
 console.log('На складі',productCount,'одиниці товару',productName,'в категорії',productCategory,'на загальну сумму',productCount*productPrice,'гривнів')
+*/
+
+const productName = prompt('Введите Наименование товара');
+const productCount = prompt('Введите количество товара');
+
+if (productCount <= Number){
+//  console.log('Вы ввели некорректные данные');
+
+  const productCategory = prompt('Введите категорию товара');
+  const productPrice = prompt('Введите цену товара');
+
+
+
+  if (productPrice<= Number){
+    console.log(productCount);
+    console.log(typeof productCount);
+    console.log(productPrice);
+    console.log(typeof productPrice);
+    console.log('На складі',productCount,'одиниці товару',productName,'в категорії',productCategory,'на загальну сумму',productCount*productPrice,'гривнів')
+  }
+
+  if (productPrice>= Number){
+    console.log('Вы ввели некорректные данные');
+  }
+}
+
+if (productCount >= Number){
+  console.log('Вы ввели некорректные данные');
+}
+
+
+
+//if (productPrice >= Number){
+//  console.log('Вы ввели некорректные данные');
+//}
+
+
+
 
