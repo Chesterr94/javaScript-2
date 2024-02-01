44b99bd26502f1fffa31220766bcc38fb44a5eaf
“Kublii <chester5800@gmail.com>
2024-02-01 23:31:59 +0200

    23.31_01.02.2024

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 console.log('index');
 
-
 const productName = prompt('Введите Наименование товара');
 
-const productCount =+ prompt('Введите количество товара');
+const productCount = +prompt('Введите количество товара');
 
-let chekin = (`Number.isNaN(${productCount}) `, Number.isNaN(productCount));
-
-if (chekin === false){
+if (!(productCount)){
+  console.log('Вы ввели некорректные данные, Шарик ты балбес');
+} else {
   const productCategory = prompt('Введите категорию товара');
-  const productPrice =+ prompt('Введите цену товара');
-  let chekinTwo = (`Number.isNaN(${productPrice}) `, Number.isNaN(productPrice));
 
-  if (chekinTwo === false){
-    console.log('На складі', productCount, 'одиниці товару', productName, 'в категорії', productCategory, 'на загальну сумму', productCount * productPrice, 'гривнів');
-  }else if (chekinTwo === true){
+  const productPrice = +prompt('Введите цену товара');
+  
+  if (!(productPrice)){
     console.log('Вы ввели некорректные данные, Шарик ты огромный балбес');
-  }
-
-  else 
-
-    if (chekin === true){
-    console.log('Вы ввели некорректные данные, Шарик ты балбес');
+  } else {
+    console.log('На складі', productCount, 'одиниці товару', productName, 'в категорії', productCategory, 'на загальну сумму', productCount * productPrice, 'гривнів');
+  };
 };
+
+
+
--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,25 +1,17 @@
-let tax0 = + prompt('Ведите сумму зароботка');
+let tax0 = +prompt('Ведите сумму зароботка');
 
-if (tax0 <= 15000){
+let income35 = 35000;
+
+if (tax0 <= income15){
   console.log('Сумма ваших налогов составляет ',tax0 / 100 * 13);
-}
-
-else if (tax0 >= 15001  && tax0 <= 50000){
-  let tax01 = (15000);
-  let tax02 = ((tax0-tax01) / 100 * 13);
-  let tax03 = ((tax0-tax02) / 100 * 20);
+} else if (tax0 >= income15  && tax0 <= income50){
+  let tax02 = (tax0-income15) / 100 * 13;
+  let tax03 = (tax0-tax02) / 100 * 20;
   console.log('Сумма ваших налогов составляет ',tax02+tax03 );
-}
-
-else
-
-  if (tax0 > 50000){
-    let tax15 = 15000;
-    let tax35 = 35000;
-    let tax01 = ((tax0-tax15) / 100 * 13);
-    let tax02 = ((tax0-tax35) /100 * 20);
-    let tax03 = ((tax0-tax15-tax35) / 100 * 30);
-
-    console.log('Сумма ваших налогов составляет ',tax01+tax02+tax03 );
-  }
+} else  if (tax0 > income50){
+  let tax01 = (tax0-income15) / 100 * 13;
+  let tax02 = (tax0-income35) /100 * 20;
+  let tax03 = (tax0-income15-income35) / 100 * 30;
+  console.log('Сумма ваших налогов составляет ',tax01+tax02+tax03 );
+};
 //второе необязательное обязательное задание
